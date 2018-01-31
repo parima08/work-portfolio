839170326de216a4018c6327a0bf9fe1ff2c6f56
Parima Shah <Parima08@gmail.com>
2018-01-30 20:16:58 -0800

    init portfolio

--- js/workcase.js
+++ js/workcase.js
@@ -1,41 +1,50 @@
-var idx = 0; 
+var idx = 1; 
 var workCases = { 
 	"workCases": [
 		{
-			name: "Micron",
-			letter: "M",  
-			shortDescription: "Lorem ipsum dolor sit amet, doctus facilisi recusabo mel ex. Te omittam maluisset has, mea agam graecis sensibus ut. Sonet placerat nec ea, te ius nobis dolorem efficiendi, ex omnes causae suscipit eam. No eam primis tritani, elit minimum", 
-			technology: [{
-					"javascript": ["jQuery"], 
-					"ruby": ["Ruby On Rails"],
-					"versionControl": ["Git", "Jenkins"]
-			}], 
-			"skill": ["Project Management", "User Experience"], 
-		},
-		{
-			name: "Cybersecurity", 
-			letter: "C",
-			shortDescription: "Lorem ipsum dolor sit amet, doctus facilisi recusabo mel ex. Te omittam maluisset has, mea agam graecis sensibus ut. Sonet placerat nec ea, te ius nobis dolorem efficiendi, ex omnes causae suscipit eam. No eam primis tritani, elit minimum", 
-			technology: [{
-					"javascript": ["jQuery", "d3.js"], 
-					"c" : ["SNMP"], 
-					"versionControl": ["Git", "Jenkins"]
-			}], 
-			"skill": ["Project Management", "Development"],
-			
-		},
-		{
-			name: "Piggybank", 
-			letter: "P",
-			shortDescription: "Lorem ipsum dolor sit amet, doctus facilisi recusabo mel ex. Te omittam maluisset has, mea agam graecis sensibus ut. Sonet placerat nec ea, te ius nobis dolorem efficiendi, ex omnes causae suscipit eam. No eam primis tritani, elit minimum", 
-			technology: [{
-					"javascript": ["jQuery", "d3.js"], 
-					"ruby": ["Ruby On Rails"],
-					"versionControl": ["Git", "Jenkins", "AWS"]
-			}], 
-			"skill": ["Project Management", "Development"],
+			name: "IoT Piggybank", 
+			subheading: "Lorem ipsum dolor sit amet, doctus facilisi recusabo mel ex. Te omittam maluisset has, mea agam graecis sensibus ut. Sonet placerat nec ea, te ius nobis dolorem efficiendi, ex omnes causae suscipit eam. No eam primis tritani, elit minimum", 
+			type: "Case Study/Ciklum",
+			role: "Lead Android Developer",
 			
 		}, 
+		{
+			name: "Smart Lock",
+			subheading: "Lorem ipsum dolor sit amet, doctus facilisi recusabo mel ex. Te omittam maluisset has, mea agam graecis sensibus ut. Sonet placerat nec ea, te ius nobis dolorem efficiendi, ex omnes causae suscipit eam. No eam primis tritani, elit minimum", 
+			type: "Case Study/Room 5", 
+			role: "Integration Server Lead"
+		},
+		{
+			name: "Network Visualization", 
+			subheading: "Lorem ipsum dolor sit amet, doctus facilisi recusabo mel ex. Te omittam maluisset has, mea agam graecis sensibus ut. Sonet placerat nec ea, te ius nobis dolorem efficiendi, ex omnes causae suscipit eam. No eam primis tritani, elit minimum", 
+			type: "Case Study/Room 5"
+			
+		},
+		{
+			name: "Micron's Developer Portal",
+		},
+		{
+			name: 'Verifone'
+		},
+		{
+			name: 'SR Love and Care'
+		}, 
+		{
+			name: 'Net Zero Enterprises'
+		},
+		{
+			name: 'Neurohacker'
+		}, 
+		{
+			name: 'Rural India - Workshop'
+		},
+		{
+			name: 'L7 Partnership'			
+		}, 
+		{
+			name: 'The Graveyard (Misc Projects)'
+		}
+		
 	],
 	"idx": function() {
 	    return idx++;
