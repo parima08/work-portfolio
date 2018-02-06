8321f3b9ed9132165f34f6bae11f73e0f5e2d99e
Parima Shah <Parima08@gmail.com>
2018-02-06 13:46:57 -0800

    everything pretty much working

--- js/main_test.js
+++ js/main_test.js
@@ -16,137 +16,246 @@ function debounce(func, wait, immediate) {
 
 $(function(){
 
-	// //CSS Changes: 
-	// //
-	// //
-	// let projectWidth = $('#project-list').width();
-	// let sortWidth = $('#sort-options').width();
-	// let windowWidth = $(window).width(); 
-	// let projectContentWidth = windowWidth - sortWidth - projectWidth; 
-	// console.log(projectContentWidth);
-
-	// $('#project-content').width(projectContentWidth); 
-
-
-    //Mustache: 
-	//
-	//
-	console.log("About to start Mustache"); 
+	var scrollMagicScenes = []; 
+	let scrollController = new ScrollMagic.Controller();
 	
-	//Project List: 
-	var projectList  =$('#project-list-template').html();
-	Mustache.parse(projectList);
-	var rendered = Mustache.render(projectList, workCases);
-	$('#project-list').html(rendered);
+	//loads the templates and draws them onto the screen
+	draw(workCases);
 
-	//Project Content:
-	console.log("About to start Mustache"); 
-	var projectContent  =$('#project-content-template').html();
-	Mustache.parse(projectContent);
-	var rendered = Mustache.render(projectContent, workCases);
-	$('.project-content-container').html(rendered);
+	//Filtering: 
+	$('a.filter-proj').on('click', function(){
+		let skillItem = this.id
+		let filteredWc = workCases.workCases.filter(function(val){
+			console.log(val.skill_set);
+			if(val.skill_set && val.skill_set.includes(skillItem)){
+				return val;
+			}
+		}); 
+		console.log(filteredWc);
+		draw({
+			"workCases": filteredWc,
+			"idx": workCases.idx, 
+			"css_class": workCases.css_class,
+		});
+	});
 
 
-	//Charming: 
-	//
-	//
-	$('.project-content-item .project-title h1').lettering('words');
+	//draws everything onto the screen.
+
+	function draw(filteredProjects){
+		//removes all the stuff
+		$('#project-list').html();
+		$('.project-content-container').html();
+
+		//rescrolls to the top of the page... 
+
+		//TODO: preloader animation? where everything switches color
+
+		//Mustache templates load everything!
+		var projectList  =$('#project-list-template').html();
+		Mustache.parse(projectList);
+		var rendered = Mustache.render(projectList, filteredProjects);
+		$('#project-list').html(rendered);
+
+		//Project Content:
+		console.log("About to start Mustache"); 
+		var projectContent  =$('#project-content-template').html();
+		Mustache.parse(projectContent);
+		var rendered = Mustache.render(projectContent, filteredProjects);
+		$('.project-content-container').html(rendered);
+
+
+		// Charming: 
+		// Seperates each "word" by putting spans around them
+		//
+		$('.project-content-item .project-title h1').lettering('words');
+
+		//calculate the timeline
+		//reintialize the controller
+		//reintialize the scrollmagic scene
+		scrollMagicCreation();
+	}
+
+	function scrollMagicCreation(){
+		let projectScrollAnimation = new TimelineMax(); 
+
+		//Overall movement of the container
+		let numOfProjects = $('.project-content-item').length; 
+		let stepLength = 100/numOfProjects; 
+		let yMove = 0,
+			timelineMove = stepLength;  
+		//let tweenAnimations = []; 
+
+		function sectionExit(currClass){
+			return [
+				new TweenMax.staggerTo(currClass + ' .project-title h1.title span', .8, {opacity: 0, y: "-100%", ease:Expo.easeOut}, .08),
+				new TweenLite.to(currClass + ' .role', .8, {opacity: 0, y: "-100%", ease:Expo.easeOut}, "-=.8"),
+				new TweenLite.to(currClass + ' .type-of-project', .4, {opacity: 0, y: "-100%", ease:Expo.easeOut}, "-=.8"),
+				new TweenLite.to(currClass + ' .subheading p', .4, {opacity: 0, y: "-10%", ease:Expo.easeOut}, "-=.8"),
+				new TweenLite.to(currClass + ' img', .2, {opacity: 0, y: "-40%", ease:Expo.easeOut}, "-=.8"),
+			];
+		// tweenAnimations.push(new TweenLite.to(currClass + ' img', .2, {opacity: 0, y: "-40%", ease:Expo.easeOut}, "-=.8"))
+		}
+
+		function sectionEnter(nextClass){
+			return [
+				new TweenLite.from(nextClass + ' img', .3, {opacity: 0, y: "20%", ease:Expo.easeOut}),
+				new TweenMax.staggerFrom(nextClass + ' .project-title h1.title span', .8, {opacity: 0, y: "100%", ease:Expo.easeOut}, .08),
+				//new TweenLite.from(nextClass + ' .project-title h1.title span', .8, {opacity: 0, y: "100%", ease:Expo.easeOut}, "-=.7"),
+				new TweenLite.from(nextClass + ' .type-of-project', .3, {opacity: 0, y: "100%", ease:Expo.easeOut}),
+				new TweenLite.from(nextClass + ' .role', .3, {opacity: 0, y: "100%", ease:Expo.easeOut}),
+				new TweenLite.from(nextClass + ' .subheading p', .4, {opacity: 0, y: "40%", ease:Expo.easeOut}),
+			];
+		 }
+
+		let contentItemIds = $('.project-content-item').map(function(index,dom){return dom.id})
+
+		for(let i = 0; i < numOfProjects -1; i++){
+			//console.log("Build Timeline - yMove: " + yMove); 
+			yMove += stepLength;
+			timelineMove += stepLength;
+			let currClass = "#" + contentItemIds[i].trim();
+			let nextClass = "#" + contentItemIds[i+1].trim();
+			let nextProjectItem = $(".project-timeline-item[data-work-case = '" + contentItemIds[i+1].trim() + "'] .project-timeline-name"); 
+			console.log(nextProjectItem);
+			projectScrollAnimation.add(sectionExit(currClass), "+=.01", "start", .01);
+			projectScrollAnimation.to(".project-content-container", .2, {y: "-" + yMove + "%"}, "-=.5"); 
+			projectScrollAnimation.to("#project-timeline", .3, {height: timelineMove + "%"}, "-=.8");
+			projectScrollAnimation.set('.project-timeline-name', {fontWeight: 400}, "-=1.1");
+			projectScrollAnimation.set(nextProjectItem, {fontWeight: 600}, "-=1.1");
+			highlightNextItems(nextClass); 
+			projectScrollAnimation.add(sectionEnter(nextClass), "-=.3", "start", .01);
+			//ADDS A DELAY of 1 second:
+			projectScrollAnimation.set({}, {}, "+=1");
+		}
+
+		//console.log(projectScrollAnimation);
+		//projectScrollAnimation.add(tweenAnimations, ".2", "start", .05);
+		//console.log("Tween Animations: " + tweenAnimations);
+		console.log("Duration is: " + projectScrollAnimation.totalDuration());
+
+		// if(scrollController){
+		// 	alert("there was a previous controller");
+		// 	scrollController = scrollController.destroy(true);
+		// }
+
+		console.log("This many scenes: " + scrollMagicScenes.length); 
+
+		if(scrollMagicScenes.length >= 1){
+			console.log("Another scroll Magic Scene exits - deleting it now");
+			scrollController.removeScene(scrollMagicScenes.pop());
+			//scrollController.destroy(true); 
+			console.log("This many scenes again: " + scrollMagicScenes.length); 
+		}
+
+		
+		console.log("ScrollController--------");
+		console.log(scrollController);
+		let duration = numOfProjects * 100 * 1.5; 
+		//let duration = numOfProjects * 100; 
+		let scrollMagicScene = new ScrollMagic.Scene({
+				triggerElement: "#layout", 
+				triggerHook: "onLeave", 
+				duration: duration + "%",
+		    })
+			.setPin('#project-content')
+			.setTween(projectScrollAnimation)
+			.addIndicators()
+			.addTo(scrollController); 
+
+		scrollMagicScenes.push(scrollMagicScene);
+
+	    scrollController.scrollTo(function (newpos) {
+	    	console.log(newpos);
+			TweenMax.to(window, 0.7, {scrollTo: { y: newpos}});
+		});
+
+		//Timeline Clicks: 
+		//
+		//
+
+		$('.project-timeline-link').on("click", function(e){
+			alert("got clicked");
+			var id = $(this).attr("href"); 
+			let numOfProjects = $('.project-content-item').length; 
+			if($(id).length > 0){
+				e.preventDefault(); 
+				console.log("The id place: " + id);
+				console.log("The position place: " + $(id).offset().top);
+
+				//TODO: Need to get this "index" value dynamically... the index
+				//of the value in the array of project-content-items. 
+
+				let indexOfId = $(this).data('index');
+				//console.log(indexOfId);
+				let docHeight = getDocHeight();
+				let scrollToPos = (docHeight/numOfProjects) * (indexOfId - 1) * (1.03)
+				//TODO: 
+				//change the tween duration to calculate overall height and divide it
+				//by the amount needed to scroll.
+				
+				//let travelSpeed = scrollToPos/docHeight * 1.2;
+				//console.log("Amount to travel:" + scrollToPos/docHeight);
+				TweenMax.to(window, .6, {scrollTo: { y: scrollToPos}});
+			}
+		});
+
+
+	}
+
+	
+
+
+
 
 	
 	//Scrolling behavior: 
 	//
-	let projectScrollAnimation = new TimelineMax(); 
-
-	//Overall movement of the container
-	let numOfProjects = $('.project-content-item').length; 
-	let stepLength = 100/numOfProjects; 
-	let yMove = 0,
-		timelineMove = stepLength;  
-	//let tweenAnimations = []; 
-
-	function sectionExit(currClass){
-		return [
-			new TweenMax.staggerTo(currClass + ' .project-title h1.title span', .8, {opacity: 0, y: "-100%", ease:Expo.easeOut}, .08),
-			new TweenLite.to(currClass + ' .role', .8, {opacity: 0, y: "-100%", ease:Expo.easeOut}, "-=.8"),
-			new TweenLite.to(currClass + ' .type-of-project', .4, {opacity: 0, y: "-100%", ease:Expo.easeOut}, "-=.8"),
-			new TweenLite.to(currClass + ' .subheading p', .4, {opacity: 0, y: "-10%", ease:Expo.easeOut}, "-=.8"),
-			new TweenLite.to(currClass + ' img', .2, {opacity: 0, y: "-40%", ease:Expo.easeOut}, "-=.8"),
-		];
-	// tweenAnimations.push(new TweenLite.to(currClass + ' img', .2, {opacity: 0, y: "-40%", ease:Expo.easeOut}, "-=.8"))
-	}
-
-	function sectionEnter(nextClass){
-		return [
-			new TweenLite.from(nextClass + ' img', .3, {opacity: 0, y: "20%", ease:Expo.easeOut}),
-			new TweenMax.staggerFrom(nextClass + ' .project-title h1.title span', .8, {opacity: 0, y: "100%", ease:Expo.easeOut}, .08),
-			//new TweenLite.from(nextClass + ' .project-title h1.title span', .8, {opacity: 0, y: "100%", ease:Expo.easeOut}, "-=.7"),
-			new TweenLite.from(nextClass + ' .type-of-project', .3, {opacity: 0, y: "100%", ease:Expo.easeOut}),
-			new TweenLite.from(nextClass + ' .role', .3, {opacity: 0, y: "100%", ease:Expo.easeOut}),
-			new TweenLite.from(nextClass + ' .subheading p', .4, {opacity: 0, y: "40%", ease:Expo.easeOut}),
-		];
-	 }
-
-	//div id: 
-	let contentItemIds = $('.project-content-item').map(function(index,dom){return dom.id})
-
-	for(let i = 0; i < numOfProjects -1; i++){
-		console.log("Build Timeline - yMove: " + yMove); 
-		yMove += stepLength;
-		timelineMove += stepLength;
-		let currClass = "#" + contentItemIds[i].trim();
-		let nextClass = "#" + contentItemIds[i+1].trim();
-		projectScrollAnimation.add(sectionExit(currClass), "+=.01", "start", .01);
-		projectScrollAnimation.to(".project-content-container", .2, {y: "-" + yMove + "%"}, "-=.5"); 
-		projectScrollAnimation.to("#project-timeline", .3, {height: timelineMove + "%"}, "-=.8");
-		projectScrollAnimation.add(sectionEnter(nextClass), "-=.3", "start", .01);
-		projectScrollAnimation.set({}, {}, "+=1");
-		//projectScrollAnimation.addCallback(completeAnimation, null, [nextClass]);
-
-		//tweenAnimations.push(new TweenLite.to(nextClass, 2, {opacity: 1}, "-=.01")); 
-		//ADD A DELAY HERE:
-
-		//tweenAnimations.push(new TweenLite.to(".project-content-container", .5, {y: "-" + yMove + "%"})); 
-	}
-
-	function completeAnimation(nextClass){
-		console.log("The Class " + nextClass + " Completed Section: " + scrollController.scrollPos());
-	}
-	console.log(projectScrollAnimation);
-	//projectScrollAnimation.add(tweenAnimations, ".2", "start", .05);
-	//console.log("Tween Animations: " + tweenAnimations);
-	console.log("Duration is: " + projectScrollAnimation.totalDuration());
-
-	let scrollController = new ScrollMagic.Controller();
-
-	let scrollMagicScene = new ScrollMagic.Scene({
-			triggerElement: "#layout", 
-			triggerHook: "onLeave", 
-			duration: "1190%",
-	    })
-		.setPin('#project-content')
-		.setTween(projectScrollAnimation)
-		.addIndicators()
-		.addTo(scrollController); 
-
-    scrollController.scrollTo(function (newpos) {
-    	console.log(newpos);
-		TweenMax.to(window, 0.7, {scrollTo: { y: newpos}});
-	});
-
-
-	//Timeline Clicks: 
 	//
-	//
-	$('.project-timeline-link').on("click", function(e){
-		var id = $(this).attr("href"); 
-		if($(id).length > 0){
-			e.preventDefault(); 
-			console.log("The id place: " + id);
-			console.log("The position place: " + $(id).offset().top);
-			TweenMax.to(window, 0.7, {scrollTo: { y: id}});
-			//scrollController.scrollTo(id); 
+
+	//TODO: Animation only when there is more than 1 project!
+
+
+	
+
+
+
+
+
+
+	//Be able to correctly calculate the positions of "paused"
+	//timeline? 
+
+
+	//Highlights on the correct data cases:
+	function highlightNextItems(activeClass){
+
+		//watches as the screen scrolls - highlight 
+		//the various skill sets
+
+		console.log("highlightNextItems");
+
+		//removes all active classes 
+		$('#skill-set-list li').removeClass("active"); 
+		let skillSet = $(activeClass).data('skill-set');
+		if(skillSet){
+			skillSet = skillSet.split(" ")
+			console.log("skillSet: " + skillSet);
+			$.each(skillSet, function(i, v){
+				$("#" + v).addClass("active");
+			})
 		}
-	});
+
+	}
+
+	function getDocHeight() {
+	    var D = document;
+	    return Math.max(
+	        D.body.scrollHeight, D.documentElement.scrollHeight,
+	        D.body.offsetHeight, D.documentElement.offsetHeight,
+	        D.body.clientHeight, D.documentElement.clientHeight
+	    )
+	}
 
 });
 
--- js/workcase.js
+++ js/workcase.js
@@ -2,17 +2,19 @@ var idx = 1;
 var workCases = { 
 	"workCases": [
 		{
+			index: 1,
 			name: "IoT Piggybank", 
-			subheading: "Lorem ipsum dolor sit amet, doctus facilisi recusabo mel ex. Te omittam maluisset has, mea agam graecis sensibus ut. Sonet placerat nec ea, te ius nobis dolorem efficiendi, ex omnes causae suscipit eam. No eam primis tritani, elit minimum", 
 			type: "Case Study/Ciklum",
 			role: "Lead Android Developer",
 			class: "active",
 			skill_set: "full-stack-dev mobile-application cloud ui-design",
 			industries: "iot",
-			image: "../img/project-bg/iot-piggybank.png"
+			subheading: "We were approached by one of America’s biggest banks with the challenge to increase their accounts with children. We developed an IoT artifact for them that engaged children and taught them basic budgetting techniques." , 
+			image: "../img/project-bg/iot-piggybank-grad.png"
 			
 		}, 
 		{
+			index: 2,
 			name: "Smart Lock",
 			subheading: "Lorem ipsum dolor sit amet, doctus facilisi recusabo mel ex. Te omittam maluisset has, mea agam graecis sensibus ut. Sonet placerat nec ea, te ius nobis dolorem efficiendi, ex omnes causae suscipit eam. No eam primis tritani, elit minimum", 
 			type: "Case Study/Room 5", 
@@ -20,9 +22,10 @@ var workCases = {
 			class: "next",
 			skill_set: "full-stack-dev cloud",
 			industries: "iot",
-			image: "../img/project-bg/smart-lock.png"
+			image: "../img/project-bg/smart-lock-grad.png"
 		},
 		{
+			index: 3,
 			name: "Network Visualization", 
 			subheading: "Lorem ipsum dolor sit amet, doctus facilisi recusabo mel ex. Te omittam maluisset has, mea agam graecis sensibus ut. Sonet placerat nec ea, te ius nobis dolorem efficiendi, ex omnes causae suscipit eam. No eam primis tritani, elit minimum", 
 			type: "Case Study/Room 5",
@@ -32,41 +35,49 @@ var workCases = {
 			
 		},
 		{
+			index: 4,
 			name: "Microns Developer Portal",
 			image: "../img/project-bg/micron.png", 
 			skill_set: "full-stack-dev ui-design",
 		},
 		{
+			index: 5,
 			name: 'Verifone',
 			image: "../img/project-bg/verifone.png",
 			skill_set: "full-stack-dev  project-management",
 		},
 		{
+			index: 6,
 			name: 'SR Love and Care',
 			image: "../img/project-bg/love-and-care.png",
 			skill_set: "full-stack-dev  project-management ui-design",
 			industries: "non-profit"
 		}, 
 		{
+			index: 7,
 			name: 'Net Zero Enterprises',
-			skill_set: "project-management CMS ui-design",
+			skill_set: "project-management cms ui-design",
 		},
 		{
+			index: 8,
 			name: 'Neurohacker',
-			skill_set: "project-management CMS",
+			skill_set: "project-management cms",
 		}, 
 		{
+			index: 9,
 			name: 'Rural India - Workshop',
 			skill_set: "project-management",
 			industries: "non-profit"
 		},
 		{
+			index: 10,
 			name: 'L7 Partnership', 
-			skill_set: "project-management CMS",
+			skill_set: "project-management cms",
 			
 		}, 
 		{
-			name: 'The Graveyard-Misc Projects-',
+			index: 11,
+			name: 'The Graveyard-Misc Projects-', 
 		}
 		
 	],
